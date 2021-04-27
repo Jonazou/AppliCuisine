df46c7952441695afa9384c56a828592b3422d6c
Jonas <jonasfazendeiro@orange.fr>
2021-04-27 16:17:49 +0200

    Mise en place de la fenetre, systeme de tabs.
    Bouton +.
    
    Style a revoir (fichier css ajouté).

--- AppliCuisine/src/main/Controller.java
+++ AppliCuisine/src/main/Controller.java
@@ -9,25 +9,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class Controller {
 	@FXML private Button test,test2,boutonRetour;
 	
-	public GestionScenes gestion = new GestionScenes();
+	@FXML private TabPane tabPane;
 	
+	@FXML private Tab newTab;
 	
 	private Stage stage;
 	private Parent parent = null;
-
 	
 	@FXML
 	public void changeScene(ActionEvent event) throws IOException {
 		if(event.getSource()==test)
-			parent = FXMLLoader.load(getClass().getResource(this.gestion.listeScenes[1]));
+			parent = FXMLLoader.load(getClass().getResource("../resources/recherche.fxml"));
 
 		else if (event.getSource()==test2)
-			parent = FXMLLoader.load(getClass().getResource(this.gestion.listeScenes[0]));
+			parent = FXMLLoader.load(getClass().getResource("../resources/accueil.fxml"));
 
 		stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
 		stage.setScene(new Scene(parent));
@@ -37,6 +39,13 @@ public class Controller {
 	@FXML
 	public void retourScene(ActionEvent event) throws IOException{
 		return;
+	}
+	
+	@FXML
+	public void addTab() {
+		Tab oui = new Tab("ayayayayayayayayayayayayayaay");
+		tabPane.getTabs().add(tabPane.getTabs().size()-1, oui);
+		tabPane.getSelectionModel().select(oui);
 	}
 	
 	
--- AppliCuisine/src/main/Fenetre.java
+++ AppliCuisine/src/main/Fenetre.java
@@ -8,15 +8,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Fenetre extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("../resources/accueil.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("../resources/tabpane.fxml"));
 		Scene scene = new Scene(root);
-
+		
+		scene.getStylesheets().add(getClass().getResource("../resources/style.css").toExternalForm());
+		
+		stage.setMinHeight(400);
+		stage.setMinWidth(300);
+		
+		stage.setTitle("oui");
 		stage.setScene(scene);
+		stage.isFullScreen();
 		stage.show();
 	}
 
